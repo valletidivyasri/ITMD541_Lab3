815cfbac11ee788207829eb23678baa3f2a1828e
ValletiDivyaReddy <79827024+ValletiDivyaReddy@users.noreply.github.com>
2023-10-29 18:06:52 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const billTotal = document.getElementById("billTotal");
+    const tipSlider = document.getElementById("tip");
+    const tipPercentage = document.getElementById("tipPercentage");
+    const tipAmount = document.getElementById("tipAmount");
+    const totalBill = document.getElementById("totalBill");
+    const tipForm = document.getElementById("tip-form");
+   
+    tipSlider.addEventListener("input", function () {
+        tipPercentage.value = tipSlider.value;    
+        tipPercentage.setCustomValidity("Tip Percentage must remain at 0 until the form is submitted");
+    });
+
+    tipForm.addEventListener("input", function () {
+        const billValue = parseFloat(billTotal.value);
+        const tipValue = parseInt(tipSlider.value);
+        if (!isNaN(billValue)) {
+            const tipAmountValue = (billValue * tipValue) / 100;
+            const totalValue = billValue + tipAmountValue;
+            tipAmount.value = tipAmountValue.toFixed(2);
+            totalBill.value = totalValue.toFixed(2);
+        } else {
+            alert('Please enter a valid number for bill total');
+            return;
+        }
+    });
+});
